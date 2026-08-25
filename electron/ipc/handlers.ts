5c1e3eaf400d1cb6490f266342fa6a8cb8a657d8
RanbeerReddy <reddyranbeer@gmail.com>
2026-08-25 19:21:50 +0530

    feat(ipc): aggregate and register all IPC handler modules

---
+++ electron/ipc/handlers.ts
@@ -0,0 +1,27 @@
+import { registerSubjectHandlers } from './subjects';
+import { registerSessionHandlers } from './sessions';
+import { registerQuestionHandlers } from './questions';
+import { registerMistakeHandlers } from './mistakes';
+import { registerRevisionHandlers } from './revisions';
+import { registerMockHandlers } from './mocks';
+import { registerPlannerHandlers } from './planner';
+import { registerGoalHandlers } from './goals';
+import { registerAnalyticsHandlers } from './analytics';
+import { registerSettingsHandlers } from './settings';
+import { registerSearchHandlers } from './search';
+import { log } from '../utils/logger';
+
+export function registerAllHandlers(): void {
+  registerSubjectHandlers();
+  registerSessionHandlers();
+  registerQuestionHandlers();
+  registerMistakeHandlers();
+  registerRevisionHandlers();
+  registerMockHandlers();
+  registerPlannerHandlers();
+  registerGoalHandlers();
+  registerAnalyticsHandlers();
+  registerSettingsHandlers();
+  registerSearchHandlers();
+  log('All IPC handlers registered.');
+}
